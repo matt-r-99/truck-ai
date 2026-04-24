df17e600353c5e33a5ea7fb82e6c96f41f29ef53
matt-r-99 <matt@mattrutledge.net>
2026-04-23 17:48:59 -0700

    scraper.py

--- scraper.py
+++ scraper.py
@@ -1,18 +1,22 @@
-
 import requests
 from bs4 import BeautifulSoup
 
 def scrape_craigslist():
-    url = "https://portland.craigslist.org/search/cto?query=4x4+truck"
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, "html.parser")
+    url = "https://portland.craigslist.org/search/cta?query=4x4+truck&min_auto_year=2003&max_auto_year=2015"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    res = requests.get(url, headers=headers, timeout=10)
+    soup = BeautifulSoup(res.text, "lxml")
 
     listings = []
 
     for item in soup.select(".result-title"):
         listings.append({
             "title": item.text,
-            "link": item["href"]
+            "link": item.get("href")
         })
 
     return listings
